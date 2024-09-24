af71ea914cd21678441fbcd962b89def3ac8e7c4
Luiza Macena Dantas <luiza.macena2013@gmail.com>
2024-09-23 22:33:37 -0300

    .

---
+++ bunny.config.js
@@ -0,0 +1,12 @@
+import BunnyIcon from './assets/bunny.svg';
+
+function App() {
+  return (
+    <div>
+      <h1>Meu Ícone de Coelho</h1>
+      <img src={BunnyIcon} alt="Coelho bonitinho" />
+    </div>
+  );
+}
+
+export default App;
