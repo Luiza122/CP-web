14742989b0a07e0c85dcec041e8b45d4011d36bd
Luiza Macena Dantas <luiza.macena2013@gmail.com>
2024-10-05 17:18:38 -0300

    rodape

--- src/Componentes/Footer/index.jsx
+++ src/Componentes/Footer/index.jsx
@@ -1,9 +1,28 @@
+import React from 'react';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'; 
+import { SiReact, SiJavascript, SiCss3, SiHtml5 } from 'react-icons/si'; 
+
 function Footer() {
   return (
     <footer>
+      <div className="social-icons">
+        <a href="https://github.com/Luiza122" target="_blank" rel="noopener noreferrer">
+          <FaGithub title="GitHub" />
+        </a>
+        <a href="https://www.instagram.com/luuh_macena_" target="_blank" rel="noopener noreferrer">
+          <FaInstagram title="Instagram" />
+        </a>
+        <a href="https://www.linkedin.com/in/luiza-macena" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin title="LinkedIn" />
+        </a>
+      </div>
+
+      
+
       <p>&copy; 2024 Luiza - Todos os direitos reservados.</p>
     </footer>
   );
 }
 
-export default Footer
+export default Footer;
+
